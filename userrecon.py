a9a3cc47fcabfdfe89ec83852a0ae116b28a0b52
ishi0n <guibs225@gmail.com>
2021-04-11 22:20:02 -0300

    instagram adicionado

--- userrecon.py
+++ userrecon.py
@@ -5,31 +5,36 @@ from sys import argv
 
 class Result:
   def __init__(self, exists: bool, status_code: int, username: str) -> None:
-      self.exists = exists
-      self.status_code = status_code
-      self.username = username
+    self.exists = exists
+    self.status_code = status_code
+    self.username = username
 
 class UserRecon:
   def __init__(self, username) -> None:
-      self.username = username
+    self.username = username
 
   def check_all(self):
-      functions = list(filter(lambda item: (item.startswith("check_") and item != "check_all"), dir(self)))
-      functions = [getattr(self, f) for f in functions]
-      
-      for f in functions:
-          ...
+    functions = list(filter(lambda item: (item.startswith("check_") and item != "check_all"), dir(self)))
+    functions = [getattr(self, f) for f in functions]
+    
+    for f in functions:
+      ...
 
   def check_facebook(self) -> bool:
-      response = requests.get("https://m.facebook.com/"+self.username)
-      
-      return Result(True if response.status_code == 200 else False, response.status_code, self.username)
+    response = requests.get("https://m.facebook.com/"+self.username)
+    
+    return Result(True if response.status_code == 200 else False, response.status_code, self.username)
 
   def check_tiktok(self) -> bool:
-      response = requests.get("https://www.tiktok.com/@"+self.username)
+    response = requests.get("https://www.tiktok.com/@"+self.username)
       
-      return Result(True if response.status_code == 200 else False, response.status_code, self.username)
+    return Result(True if response.status_code == 200 else False, response.status_code, self.username)
 
+  def instagram(self) -> bool:
+    response = requests.get("https://www.instagram.com/"+self.username)
+    
+    return Result(True if response.status_code == 200 else False, response.status_code, self.username)
+    
 
 banner="""
  █    ██   ██████ ▓█████  ██▀███   ██▀███  ▓█████  ▄████▄   ▒█████   ███▄    █ 
